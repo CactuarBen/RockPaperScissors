6fe74e8e6583258faa73271d22033d45d6b51c4c
NIKOLAY FEDOTOV <nikolayvhs@gmail.com>
2023-05-28 17:29:35 +0200

    Finished User Interface
    -All buttons are now functional
    -The game starts upon opening the website (computerChoice)
    -Each button pressed runs game once and logs results
    -Results are dynamically updated and are shown
    -The message with the winner finishes the round

--- script.js
+++ script.js
@@ -9,22 +9,25 @@ function getComputerChoice(){
     let computerChoice = gameValues[Math.floor((Math.random()*3))];
 }
 
-function getPlayerChoice(){
-    let playerChoice = prompt("What would you choose? Rock, Paper or Scissors?")
-}
+function playGame(playerChoice){
+    let computerChoice = gameValues[Math.floor((Math.random()*3))];
+    console.log(computerChoice)
+    console.log(playerChoice)
 
-function playGame(playerChoice, computerChoice){
     if (computerChoice===playerChoice){
         roundWinner = "Draw, both values are the same"
-    } 
-    if ((computerChoice==="rock" && playerChoice==="paper") || (computerChoice==="paper" && playerChoice==="scissors") || (computerChoice==="scissors" && playerChoice==="rock")){
+    } else if ((computerChoice==="rock" && playerChoice==="paper") || (computerChoice==="paper" && playerChoice==="scissors") || (computerChoice==="scissors" && playerChoice==="rock")){
         playerScore++
         roundWinner = 'player'
-    }
-    if ((computerChoice==="paper" && playerChoice==="rock") || (computerChoice==="scissors" || playerChoice==="paper") || (computerChoice==="rock" && playerChoice==="scissors")){
+        console.log("player won")
+    } else if ((computerChoice==="paper" && playerChoice==="rock") || (computerChoice==="scissors" || playerChoice==="paper") || (computerChoice==="rock" && playerChoice==="scissors")){
         computerScore++
         roundWinner = 'computer'
-    }
+        console.log("computer won")
+    } 
+    robotScore.innerText = "Current Score is "+ computerScore;
+humanScore.innerText = "Current Score is "+ playerScore;
+    isGameOver();
 }
 
 function  isGameOver(){
@@ -35,10 +38,36 @@ function  isGameOver(){
     }
 }
 
-const playerChoice = "rock";
-const computerChoice = getComputerChoice();
-console.log(playGame(playerChoice, computerChoice));
+
 // User Interface Mechanics
+
+const rockButton = document.querySelector("#rockBtn")
+const paperButton = document.querySelector("#paperBtn")
+const scissorsButton = document.querySelector("#scissorsBtn")
+const robotScore = document.querySelector("#score-pc")
+const humanScore = document.querySelector("#score-person")
+
+robotScore.innerText = "Current Score is "+ computerScore;
+humanScore.innerText = "Current Score is "+ playerScore;
+
+rockButton.addEventListener('click', function (e) {
+    //console.log(e.target);
+    //console.log("logs rock")
+    playGame("rock")
+});
+
+paperButton.addEventListener('click', function (e) {
+    //console.log(e.target);
+    //console.log("logs paper")
+    playGame("paper")
+});
+
+scissorsButton.addEventListener('click', function (e) {
+    //console.log(e.target);
+    //console.log("logs scissors")
+    playGame("scissors")
+});
+
 //rockBtn.addEventListener('click', () => handleClick('ROCK'))
 //paperBtn.addEventListener('click', () => handleClick('PAPER'))
 //scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'))
